d072635745d5d4356217b4113f01b2c95d3d9c29
EC2 Default User <ec2-user@ip-172-31-7-62.us-east-2.compute.internal>
2021-03-08 03:35:42 +0000

    m

--- resources/views/fb/pp.blade.php
+++ resources/views/fb/pp.blade.php
@@ -13,7 +13,7 @@
         <!--<h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>-->
         <h1>遂に解禁された新情報とは!?<br><span class="red">『時給１万円〜』</span>も夢じゃない?!</h1>
         <div class="button">
-            <a href="https://lin.ee/LAceIGG" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
+            <a href="https://lin.ee/akIVFGh" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
         <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活</span>を紹介中!</p>
@@ -26,12 +26,12 @@
         <img src="{{ asset('image/p_manga_3.jpg') }}"></img>
         <img src="{{ asset('image/p_manga_4.jpg') }}"></img>
         <h1>初心者でも大丈夫! <span class="red">むしろ初心者向け！</span><br>パパ見つけやすさに定評あり!</h1>
-        <a href="https://lin.ee/LAceIGG" target="_blank" class="btn-square-little-rich"><img src="{{ asset('image/line_friend_add.png') }}"></img></a>
+        <a href="https://lin.ee/akIVFGh" target="_blank" class="btn-square-little-rich"><img src="{{ asset('image/line_friend_add.png') }}"></img></a>
         <br>
         <br>
         <div class="button">
             <!--<a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>-->
-            <a href="https://lin.ee/LAceIGG" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
+            <a href="https://lin.ee/akIVFGh" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
         <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活</span>を紹介中!</p>
@@ -44,7 +44,7 @@
         <br>
         <div class="button">
             <!--<a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>-->
-            <a href="https://lin.ee/LAceIGG" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
+            <a href="https://lin.ee/akIVFGh" target="_blank" class="btn-square-little-rich">公式LINEサポート <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活</span>を紹介中!</p>
         <img src="{{ asset('image/campaign.png') }}">
