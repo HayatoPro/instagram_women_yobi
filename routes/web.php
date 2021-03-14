ce83e1482064f700a56205b3e7f07331dfd70aea
EC2 Default User <ec2-user@ip-172-31-7-62.us-east-2.compute.internal>
2021-03-14 12:06:32 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//LP獲得用
-Route::get('special', function () {
-    return view('fb.cccheck'); //LP提出用
-    //リダイレクトLPへ切り替え - cccheckを変更
+// //LP獲得用
+// Route::get('special', function () {
+//     return view('fb.cccheck'); //LP提出用
+//     //リダイレクトLPへ切り替え - cccheckを変更
     
-    // return view('fb.pp'); //獲得LP(2) - パパ活
-    // return view('fb.job'); //獲得LP(1)
-});
+//     // return view('fb.pp'); //獲得LP(2) - パパ活
+//     // return view('fb.job'); //獲得LP(1)
+// });
 
 //OS識別
-Route::get('cccheck', 'AdController@os');
+Route::get('special', 'AdController@os');
 
 // //LP確認用
 // Route::get('cccheck', function () {
