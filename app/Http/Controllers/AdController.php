6d4125c1e22156f7f5c7d2e5ad97361f89d0a7e1
EC2 Default User <ec2-user@ip-172-31-7-62.us-east-2.compute.internal>
2021-03-31 05:03:50 +0000

    M

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,12 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+            return redirect('https://a-trade.jp/redirect/anahiro1?media=G16531'); //アナザーヒロイン - Aトレード
+            // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
             // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
         }
-        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
+        return redirect('https://a-trade.jp/redirect/anahiro1?media=G16531'); //アナザーヒロイン - Aトレード
+        // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         // return redirect('https://a-trade.jp/redirect/giftchat?media=G16531'); //ギフトチャット - Aトレード
     }
 }
