be6b371ea4b8f5c717a9f6b2d40f5b9ce8c7631e
EC2 Default User <ec2-user@ip-172-31-7-62.us-east-2.compute.internal>
2021-04-01 03:42:40 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,12 +10,14 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/anahiro1?media=G16531'); //アナザーヒロイン - Aトレード
-            // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+            return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
             // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
+            // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
+            // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+            // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活iOS - fam
         }
-        return redirect('https://a-trade.jp/redirect/anahiro1?media=G16531'); //アナザーヒロイン - Aトレード
-        // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
+        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         // return redirect('https://a-trade.jp/redirect/giftchat?media=G16531'); //ギフトチャット - Aトレード
+        // return redirect('https://fam-ad.com/ad/p/r?_site=64802&_article=16345'); //ワクワクP活web - fam
     }
 }
